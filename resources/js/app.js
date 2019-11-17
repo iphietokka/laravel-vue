b484f0eae71dd3aaf33b87ee78d491fc6bad5b9d
iphietokka <campurcampur4@gmail.com>
2019-11-18 02:59:08 +0700

    create a basic vue js component

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,8 @@
 require('./bootstrap');
 require('./fontawesome');
 
+
+
 window.Vue = require('vue');
 
 /**
@@ -19,8 +21,7 @@ window.Vue = require('vue');
 
 // const files = require.context('./', true, /\.vue$/i);
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('user-info', require('./components/UserInfo.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
