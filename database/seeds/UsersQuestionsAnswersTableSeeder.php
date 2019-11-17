cba8859334ac9555e625ead217daa3f29040555b
iphietokka <campurcampur4@gmail.com>
2019-11-18 01:57:15 +0700

    tidyng up the view

--- database/seeds/UsersQuestionsAnswersTableSeeder.php
+++ database/seeds/UsersQuestionsAnswersTableSeeder.php
@@ -14,14 +14,14 @@ class UsersQuestionsAnswersTableSeeder extends Seeder
         \DB::table('answers')->delete();
         \DB::table('questions')->delete();
         \DB::table('users')->delete();
-        factory(App\User::class, 3)->create()->each(function ($u) {
+        factory(App\User::class, 5)->create()->each(function ($u) {
             $u->questions()
                 ->saveMany(
-                    factory(App\Question::class, rand(1, 5))->make()
+                    factory(App\Question::class, rand(1, 10))->make()
 
                 )
                 ->each(function ($q) {
-                    $q->answers()->saveMany(factory(App\Answer::class, rand(1, 5))->make());
+                    $q->answers()->saveMany(factory(App\Answer::class, rand(1, 10))->make());
                 });
         });
     }
