3f0efb7c2389c408c02d59a44079fc24a6090cf6
iphietokka <campurcampur4@gmail.com>
2020-01-14 21:35:12 +0700

    create answers component and implement vue list rendering

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -32,10 +32,8 @@
             </div>
         </div>
     </div>
-    @include('answers._index', [
-        'answers' => $question->answers,
-        'answersCount' => $question->answers_count,
-    ])
+<answers :answers="{{$question->answers}}" :count="{{$question->answers_count}}"></answers>
+
     @include('answers._create')
 </div>
 @endsection
