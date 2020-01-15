1ab084db57ed8ecb421e515d775bc8a6a973fc1f
iphietokka <campurcampur4@gmail.com>
2020-01-16 04:52:23 +0700

    Create markdown component editor

--- resources/js/components/Question.vue
+++ resources/js/components/Question.vue
@@ -10,7 +10,9 @@
           <div class="media">
             <div class="media-body">
               <div class="form-group">
-                <textarea rows="10" v-model="body" class="form-control"></textarea>
+                <m-editor :body="body">
+                  <textarea rows="10" v-model="body" class="form-control"></textarea>
+                </m-editor>
               </div>
               <button class="btn btn-primary" :disabled="isInvalid">Update</button>
               <button class="btn btn-outline-secondary" @click="cancel" type="button">Cancel</button>
@@ -63,13 +65,14 @@
 import Vote from "./Vote.vue";
 import UserInfo from "./UserInfo.vue";
 import modification from "../mixins/modification";
+import MEditor from "./MEditor.vue";
 
 export default {
   props: ["question"],
 
   mixins: [modification],
 
-  components: { Vote, UserInfo },
+  components: { Vote, UserInfo, MEditor },
 
   data() {
     return {
