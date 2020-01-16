fb5f01f6dc78cfe77a9b863a7c346943b42686ed
iphietokka <campurcampur4@gmail.com>
2020-01-16 15:27:17 +0700

    Make the tab functional

--- resources/js/components/MEditor.vue
+++ resources/js/components/MEditor.vue
@@ -1,26 +1,39 @@
 <template>
-  <div class="card">
-    <div class="card-header">
-      <ul class="nav nav-tabs card-header-tabs">
-        <li class="nav-item">
-          <a class="nav-link active" data-toggle="tab" href="#write">Write</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="tab" href="#preview">Preview</a>
-        </li>
-      </ul>
+    <div class="card">
+        <div class="card-header">
+            <ul class="nav nav-tabs card-header-tabs">
+                <li class="nav-item">
+                    <a class="nav-link active" data-toggle="tab" href="#write"
+                        >Write</a
+                    >
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-toggle="tab" href="#preview"
+                        >Preview</a
+                    >
+                </li>
+            </ul>
+        </div>
+        <div class="card-body tab-content">
+            <div class="tab-pane active" id="write">
+                <slot></slot>
+            </div>
+            <div class="tab-pane" id="preview" v-html="preview"></div>
+        </div>
     </div>
-    <div class="card-body tab-content">
-      <div class="tab-pane active" id="write">
-        <slot></slot>
-      </div>
-      <div class="tab-pane" id="preview">Preview...</div>
-    </div>
-  </div>
 </template>
 
 <script>
+import MarkdownIt from "markdown-it";
+const md = new MarkdownIt();
+
 export default {
-  props: ["body"]
+    props: ["body"],
+
+    computed: {
+        preview() {
+            return md.render(this.body);
+        }
+    }
 };
 </script>
